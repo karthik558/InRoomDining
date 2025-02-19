5f5748f005c6d40a7431d54c489bfce9f118e1cf
KARTHIK LAL <karthik.lal558@gmail.com>
2025-02-19 22:03:23 +0530

    website/ird: Remove unused Next.js build files and cache
    Signed-off-by: KARTHIK LAL karthik.lal558@gmail.com

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/MAC/Repository/InRoomDining/app/layout\":[\"static/media/9dd75fadc5b3df29-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
